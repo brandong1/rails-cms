7494b184373caaed9a9dd5c0968ce782e68d57fa
brandong1 <brandon@brandonleeproductions.com>
2019-04-13 23:31:10 -0600

    create index template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   # Root Route
-  root "demo#index"
+  root "subject#index"
 
   # This code block creates all 8 routes for our SubjectController
   resources :subjects do
